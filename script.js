0e630d2bf94484fc21e3b4a170a2f07fb4c1d651
HanzlaKhalid <53270755+HanzlaKhalid@users.noreply.github.com>
2019-07-25 18:26:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var name = prompt ("Whats your name?");
+var fatherName = prompt ("Whats your father name?");
+var address = prompt("Enter your living place?");
+var rollNo = prompt ("Enter your Roll No#");
+alert ("Your name is "+name+", Your father name is "+fatherName+", Your address is "+address+", Your roll no is "+rollNo);
+var a = 100;
+if (a < rollNo) {
+    alert ("You are new student")
+}else{
+    alert ("You are old student")
+}
